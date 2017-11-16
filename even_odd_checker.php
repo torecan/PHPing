1d34dc8826f16b515b16536b33b27dad9a53bb00
torecan <torecancelik@gmail.com>
2017-11-17 00:19:48 +0100

    even_odd_checker.php

---
+++ even_odd_checker.php
@@ -0,0 +1,21 @@
+<?php
+
+//Even or Odd program
+
+$value=100;
+
+
+if($value%2==0){
+
+
+echo << "Provided number is ". $value ."is even";
+
+}
+else{
+
+echo << "Provided number: ". $value ." is odd";
+
+}
+
+
+?>
